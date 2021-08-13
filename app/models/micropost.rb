f6dbd77f1c0e91d6af54b2b0e7e21bb16981f8e6
Yasuhiro Sugawara <toreharose@gmail.com>
2021-08-13 17:18:03 +0900

    Set a order of micrposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,4 @@
 class Micropost < ApplicationRecord
+  default_scope -> { order(created_at: :desc)}
   validates :content, presence: true, length: { maximum: 140}
 end
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -2,14 +2,26 @@ require "test_helper"
 
 class MicropostTest < ActiveSupport::TestCase
 
+  def setup
+    @micropost = Micropost.new(content: "Lorem ipsum")
+  end
+
   test "should be valid" do
-    micropost = Micropost.new(content: "Lorem ipsum")
-    assert micropost.valid?
+    assert @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = " "
+    assert_not @micropost.valid?
   end
   
-  test "shoud be invalid" do
-    micropost = Micropost.new(content: "a" * 141)
-    assert_not micropost.valid?
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
   end
 
 end
