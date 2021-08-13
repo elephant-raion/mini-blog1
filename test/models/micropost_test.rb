ecea04e94514c70bc58be9eaa542044388721dca
Yasuhiro Sugawara <toreharose@gmail.com>
2021-08-13 11:23:55 +0900

    Define a model as micropost

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,12 +1,15 @@
 require "test_helper"
 
 class MicropostTest < ActiveSupport::TestCase
-  def setup
-    @micropost = Micropost.new(content: "Lorem ipsum")
-  end
 
   test "should be valid" do
-    assert @micropost.valid?
+    micropost = Micropost.new(content: "Lorem ipsum")
+    assert micropost.valid?
   end
   
+  test "shoud be invalid" do
+    micropost = Micropost.new(content: "a" * 141)
+    assert_not micropost.valid?
+  end
+
 end
