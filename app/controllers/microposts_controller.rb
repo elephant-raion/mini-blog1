95d049a5b33248e12a047c9906e1155684835f8b
Yasuhiro Sugawara <toreharose@gmail.com>
2021-08-14 12:06:24 +0900

    Micropostsの一覧を表示でき、Micropostを投稿できる。

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,19 @@
 class MicropostsController < ApplicationController
+
+  def create
+    @micropost = Micropost.new(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+    end
+    redirect_to root_url
+  end
+
+  def index
+    @feed_items = Micropost.all.paginate(page: params[:page])
+  end
+
+  private
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'microposts#index'
+  resources :microposts, only: [:index, :create]
 end
