81d50130863811bafdad6c111fbaaf3168e07986
Yasuhiro Sugawara <toreharose@gmail.com>
2021-08-18 20:04:08 +0900

    Add a controller of static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'microposts#index'
+  root 'pages#index'
+  get 'pages/show'
   resources :microposts, only: [:index, :create]
 end
