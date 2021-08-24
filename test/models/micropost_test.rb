1799da57efd4bd4b26b8b79f045a0235c1649ae1
Yasuhiro Sugawara <toreharose@gmail.com>
2021-08-25 05:31:23 +0900

    UserとMicropostの必ず関係があることを確認するテストを追加した

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,11 +3,17 @@ require "test_helper"
 class MicropostTest < ActiveSupport::TestCase
 
   def setup
-    @micropost = Micropost.new(content: "Lorem ipsum")
+    @user = users(:yamada)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
     assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
   end
 
   test "content should be present" do
