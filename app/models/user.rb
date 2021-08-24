f7e187613121e88c664d0174e073e340375f3c69
Yasuhiro Sugawara <toreharose@gmail.com>
2021-08-24 23:55:58 +0900

    User modelにname, profile, urlを追加し、そのmodelの単体テストを行なった

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_many :microposts
+  validates :name, presence: true, length: {maximum: 20}, format: {with: /\A[a-zA-Z]+\z/}
+  validates :profile, length: {maximum: 200}
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      name: "yamada",
+      email: "hoge@google.com",
+      password: "foobarfoobar",
+      profile: "I work for my life.",
+      url: "https://github.com/elephant-raion"
+    )
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name's length should be within 20" do
+    @user.name = "a"*21
+    assert_not @user.valid?
+  end
+
+  test "name should include alphabet only" do
+    @user.name = "takeshi tanaka"
+    assert_not @user.valid?
+  end
+
+  test "profile should be within 200" do
+    @user.profile = "a"*201
+    assert_not @user.valid?
+  end
+  
 end
