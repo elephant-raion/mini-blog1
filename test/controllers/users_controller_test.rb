c6aaf85de22e878ede6320eca150f6b28f082e0e
Yasuhiro Sugawara <toreharose@gmail.com>
2021-09-02 22:30:29 +0900

    ユーザページが表示されることを確かめるテストを追加した

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:yamada)
+  end
+
   test "should get show" do
-    get users_show_url
-    assert_response :success
+    get "/users/show/#{@user.id}"
+    assert_template 'users/show'
   end
 end
