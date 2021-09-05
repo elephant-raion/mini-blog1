8c1f924115c4ea50791e9c22d3b93c0cf580f0c9
Yasuhiro Sugawara <toreharose@gmail.com>
2021-09-06 07:52:51 +0900

    Generate user controller with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }  
   resources :users, only: [:show]
   resources :microposts, only: [:index, :create]
 end
