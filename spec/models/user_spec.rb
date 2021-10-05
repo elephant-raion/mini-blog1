2f1b88290a1a165f93bb111a53e67235b910434e
Yasuhiro Sugawara <toreharose@gmail.com>
2021-10-06 06:53:38 +0900

    specで使うmodelをletで生成するようにした

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  before do
-    @user = FactoryBot.build(:user)
-  end
+  let(:user) { create(:user)}
 
   it "is valid with a name, email, profile, url, and password" do
-    expect(@user).to be_valid
+    expect(user).to be_valid
   end
 
   it "is invalid without a name" do
-    @user.name = nil
-    expect(@user).to be_invalid
-    expect(@user.errors).to be_of_kind(:name, :blank)
+    user.name = nil
+    expect(user).to be_invalid
+    expect(user.errors).to be_of_kind(:name, :blank)
   end
 
   describe "name's length" do
     context "when the length is 20 or less" do
       it "is valid" do
-        @user.name = "a"*20
-        expect(@user).to be_valid
+        user.name = "a"*20
+        expect(user).to be_valid
       end
     end
     
     context "when the length is 21 or more" do
       it "is invalid" do
-        @user.name = "a"*21
-        expect(@user).to be_invalid
-        expect(@user.errors).to be_of_kind(:name, :too_long)
+        user.name = "a"*21
+        expect(user).to be_invalid
+        expect(user.errors).to be_of_kind(:name, :too_long)
       end
     end
   end
@@ -36,16 +33,16 @@ RSpec.describe User, type: :model do
   describe "name's characters" do
     context "when the characters are alphabet only" do
       it "is valid" do
-        @user.name = "takeshi"
-        expect(@user).to be_valid
+        user.name = "takeshi"
+        expect(user).to be_valid
       end
     end
 
     context "when the characters are not alphabet only" do
       it "is invalid" do
-        @user.name = "takeshi tanaka"
-        expect(@user).to be_invalid
-        expect(@user.errors).to be_of_kind(:name, :invalid)
+        user.name = "takeshi tanaka"
+        expect(user).to be_invalid
+        expect(user.errors).to be_of_kind(:name, :invalid)
       end
     end
   end
@@ -53,16 +50,16 @@ RSpec.describe User, type: :model do
   describe "profile's length" do
     context "when the length is 200 or less" do
       it "is valid" do
-        @user.profile = "a"*200
-        expect(@user).to be_valid
+        user.profile = "a"*200
+        expect(user).to be_valid
       end
     end
     
     context "when the length is 201 or more" do
       it "is invalid" do
-        @user.profile = "a"*201
-        expect(@user).to be_invalid
-        expect(@user.errors).to be_of_kind(:profile, :too_long)
+        user.profile = "a"*201
+        expect(user).to be_invalid
+        expect(user.errors).to be_of_kind(:profile, :too_long)
       end
     end
   end
