3a1296fb879578210689ef26bfc239305d3b342d
Yasuhiro Sugawara <toreharose@gmail.com>
2021-10-24 16:32:12 +0900

    フォローしているユーザーを記憶するようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
 
   def index
-    @micropost = current_user.microposts.build if user_signed_in?
+    if user_signed_in? then
+      @micropost = current_user.microposts.build
+      @following_users = current_user.following_users
+    end
     @feed_items = Micropost.all.paginate(page: params[:page])
   end
 end
